296c694b7c8366922b5480bbce4b7cb4163746d2
Luis Dourado <lfaddourado@gmail.com>
2024-06-09 20:16:08 -0300

    fix(auth): fix adapter and values that were being passed to it

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,11 +1,11 @@
 import { Lucia } from 'lucia';
 import { DrizzleSQLiteAdapter } from '@lucia-auth/adapter-drizzle';
 import { dev } from '$app/environment';
-import * as schema from './db/schema';
-import { sessionTable } from './db/schema';
+import { sessionTable, users } from './db/schema';
+
 import { db } from './db/db';
 
-const adapter = new DrizzleSQLiteAdapter(db, schema, sessionTable);
+const adapter = new DrizzleSQLiteAdapter(db, sessionTable, users);
 
 export const lucia = new Lucia(adapter, {
 	sessionCookie: {
